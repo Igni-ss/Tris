361018fac8e3cd2f960aea42a35bb84fc56ce53d
LeleValore <emanuelevalore@gmail.com>
2026-01-13 08:38:33 +0100

    test(main): set main for test

--- main.py
+++ main.py
@@ -13,37 +13,28 @@ def main():
     ui.show_message("BENVENUTO A TRIS IMBATTIBILE")
     ui.show_message("Tu sei la X, il PC è la O")
     
-    current_player = PLAYER_X # Inizia l'umano
+    current_player = PLAYER_X 
     
     while True:
         ui.display_board(game.grid)
-        
-
         winner = game.check_winner()
         if winner:
             ui.show_message(f"PARTITA FINITA! Ha vinto: {winner}")
             break
-        
         if game.is_full():
             ui.show_message("PARTITA FINITA! Pareggio.")
             break
-
         if current_player == PLAYER_X:
             row, col = ui.get_player_move()
-            
             if not game.make_move(row, col, PLAYER_X):
                 ui.show_error("Mossa non valida o cella occupata! Riprova.")
-                continue # Salta il cambio turno, tocca ancora all'umano
-                
+                continue # Salta il cambio turno, tocca ancora all'umano 
         else:
-
             ui.show_message("Il PC sta pensando...")
             row, col = get_best_move(game)
             game.make_move(row, col, PLAYER_O)
             ui.show_message(f"Il PC ha giocato in {row} {col}")
-
         current_player = PLAYER_O if current_player == PLAYER_X else PLAYER_X
-    
     ui.display_board(game.grid)
 
 if __name__ == "__main__":
